a514a22a94c7c4a1b0dc6e2a510e0d54c38ad71d
Drake224025 <sasithamare@gmail.com>
2022-11-11 20:04:00 +0530

    refactor: add level 1 auth

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("user", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app
+  .route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }, (err, docs) => {
+      if (!err) {
+        if ((docs, password === password)) res.render("secrets");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+app
+  .route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post((req, res) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password,
+    });
+
+    User.create(newUser, (err, doc) => {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
